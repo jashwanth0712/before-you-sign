52800451090329c47c00cc3ea105434c7116cf0e
rp-ram <ramachakradharrp@gmail.com>
2023-08-26 13:17:49 +0530

    adding animation

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
+import logo from '../assets/images/giphy.gif'
+import './Homoe.js'
+
 export default function Home() {
     return (
         <div>
             {/* Edit below code */}
             <h1 className="font-grotesk text-text-primary text-center mt-20 max-w-4xl mx-auto">Join over 700 million registered users who trust Dropbox</h1>
             <p className="font-grotesk font-medium text-xl underline text-text-primary text-center mt-20">Dropbox Design Colors</p>
+            <p className="font-grotesk text-xs text-center">Using the sign API's to make the long aggrements short and only the important points and also it will make clarify your doubts about most of the legal questions.</p>
+
 
             <div className="p-20 flex flex-wrap gap-3">
                 <div className="text-xs font-medium h-28 w-28 grid place-content-center bg-accent-light">Accent Light</div>
@@ -18,6 +24,11 @@ export default function Home() {
                 <div className="text-xs font-medium h-28 w-28 grid place-content-center bg-dropbox-orchid">Orchid</div>
                 <div className="text-xs font-medium h-28 w-28 grid place-content-center bg-dropbox-pink">Pink</div>
             </div>
+
+            <section className="show-animate-{transform -translate-x-0}">
+                <div className="flex justify-center items-center"><img src={logo} /></div>
+                <p className="font-grostek text-xl transform -translate-x-full text-center mt-4 transition duration-1000 min-h-screen">The above GIF tells you how the important points are getting highlighted.</p>
+            </section>
         </div>
     );
 }
---
+++ website/src/pages/Homoe.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+            let top =window.scrollY;
+            let offset = sec.offsetTop;
+            let height = sec.offsetHeight;
+
+            if(top > offset && top <offset+height)
+            {
+                sec.classList.add('show-animate');
+            }
+            else
+            {
+                sec.showList.remove('show-animate');
+            }
+        }
+    );
+}
