13a7b6737430fe5a7b517988f05a27d5bbae5093
Himanshu Malviya <malviya.himansh@gmail.com>
2023-10-02 12:46:41 +0530

    minor design changes

--- extension/src/components/Chat.js
+++ extension/src/components/Chat.js
@@ -4,7 +4,7 @@ const Chat = ({response, text}) => {
             <div className={`profile shadow-2xl rounded-full w-10 h-10 bg-white border ${response ? 'border-primaryGreen' : 'border-primaryPurple'} grid place-content-center`}>
                 <p className="text-lg">{response ? '🤖' :  '🧑🏻‍🦱'}</p>
             </div>
-            <div className={`message shadow-2xl rounded-3xl ${response ? 'bg-green-100' : 'bg-purple-500 text-white'} text-sm sm:text-base p-4 px-5 max-w-[300px] sm:max-w-[300px]`}>
+            <div className={`message shadow-2xl rounded-3xl ${response ? 'bg-green-100' : 'bg-purple-400 text-white'} text-sm sm:text-base p-4 px-5 max-w-[300px] sm:max-w-[300px]`}>
                 <p>{text}</p>
             </div>
         </div>
